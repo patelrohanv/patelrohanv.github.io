32db38e6baebf713eeaedf88a651f99723b01d55
patelrohanv <patelrohanv@gmail.com>
2017-03-14 12:33:59 -0400

    Updated js

--- js/main.js
+++ js/main.js
@@ -11,9 +11,12 @@ $('.parallax').parallax({
 
 });
 
-$('.button-collapse').sideNav({
-    menuWidth: 300, // Default is 300
-    edge: 'left', // Choose the horizontal origin
-    closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
-    draggable: true // Choose whether you can drag to open on touch screens
-});
+// Initialize collapse button
+$(".button-collapse").sideNav();
+
+// Show sideNav
+$('.button-collapse').sideNav('show');
+// Hide sideNav
+$('.button-collapse').sideNav('hide');
+// Destroy sideNav
+$('.button-collapse').sideNav('destroy');
