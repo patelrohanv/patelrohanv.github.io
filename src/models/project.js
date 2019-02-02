dabad04859e2deb206623e7c9a2e16da78a3ed59
Rohan <patelrohanv@gmail.com>
2019-02-02 18:20:41 -0500

    finally finished data entry

---
+++ src/models/project.js
@@ -0,0 +1,37 @@
+export var project = [
+    {
+        title: "Steel Hacks 2017",
+        info: "We created a TwitterBot that would search for tweets containing profanity and a video game title. We would then sent a message to the user who made that tweet saying to be nicer on the Internet. This Bot won the #HackHarassment category award.",
+        gitLink: "https://github.com/patelrohanv/steelhacks2017",
+        projectLink: "https://devpost.com/software/hack-harassment-for-twitter",
+    },
+    {
+        title: "Laboon Chess",
+        info: "This is a group project for the Fall 2016 CS1530 Software Engineering course. We worked as a group of 4 to create and deliever a functioning chess program. The work for this project was split over five deliverables and we working on this using the Agile Scrum methodology and we used Gradle as our build tool to build, test and run our program.",
+        gitLink: "https://github.com/patelrohanv/CS-1530-Software-Engineering",
+        projectLink: null,
+    }, {
+        title: "TinyGoogle - Hadoop",
+        info: "This is an implementation of a tinyGoogle search engine using Hadoop MapReduce that readings in text files and allows a user to search for term(s) and get an output of the documents that contain each term ordered by the count in descending order. This also has the functionality to add and index new text files too.",
+        gitLink: "https://github.com/patelrohanv/tinygoogle-hadoop",
+        projectLink: null,
+    }
+    , {
+        title: "TinyGoogle - Spark",
+        info: "This is an implementation of a tinyGoogle search engine using Spark that readings in text files and allows a user to search for term(s) and get an output withhow many times that term appears in each text file and how that number is weighted relative to the other documents.",
+        gitLink: "https://github.com/patelrohanv/tinygoogle-spark",
+        projectLink: null,
+    },
+    {
+        title: "patelrohanv.com",
+        info: "This is a link to the repo for this built.",
+        gitLink: "https://github.com/patelrohanv/patelrohanv.github.io",
+        projectLink: null,
+    },
+    {
+        title: "Pitt SigRho Website",
+        info: "I built and maintain the website for the University of Pittsburgh chapter of Sigma Beta Rho Fraternity Inc. I gigggbuilt this website using Twitter's Bootstrap framework",
+        gitLink: "https://github.com/sigrhopitt/sigrhopitt.github.io",
+        projectLink: "https://sigrhopitt.github.io"
+    }
+]
