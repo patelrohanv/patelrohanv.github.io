4a895c202bcc8e220b60f2a9b2538f63a8b0c1ab
rohan <patelrohanv@gmail.com>
2019-02-07 07:29:02 -0500

    aded background

--- src/App.js
+++ src/App.js
@@ -9,20 +9,29 @@ import Projects from './components/projects';
 import Undergrad from './components/undergrad';
 import Footer from './components/footer';
 
+import background from './images/desk_light_l.jpeg'
+
 class App extends Component {
   render() {
+    const styles = {
+      main: {
+        backgroundImage: 'url(' + background + ')',
+      },
+    }
     return (
       <div className="App">
         <Header />
-        <Skills />
-        <hr/>
-        <Education />
-        <hr/>
-        <Experience />
-        <hr/>
-        <Projects />
-        <hr/>
-        <Undergrad />
+        <div style= {styles.main}>
+          <Skills />
+          <hr />
+          <Education />
+          <hr />
+          <Experience />
+          <hr />
+          <Projects />
+          <hr />
+          <Undergrad />
+        </div>
         <Footer />
       </div>
     );
