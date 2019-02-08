b0924a17180731c86eefd1618f18ec5f3c0f420e
rohan <patelrohanv@gmail.com>
2019-02-07 22:49:22 -0500

    Updates

---
+++ precache-manifest.ed55ea0d52c713c135a3ca2aaead72dc.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "6d4dd2fd2ef4f5ee7ad35db29aab75a9",
+    "url": "patelrohanv.com/static/media/headshot.6d4dd2fd.jpg"
+  },
+  {
+    "revision": "dfecbb8591923ca22621a76d6a1ad3dc",
+    "url": "patelrohanv.com/static/media/desk_light_l.dfecbb85.jpeg"
+  },
+  {
+    "revision": "d7c071b4fc64d8d9bf8e78b6150b8a47",
+    "url": "patelrohanv.com/static/media/code_m.d7c071b4.jpeg"
+  },
+  {
+    "revision": "4d655a95cb868771e5aa",
+    "url": "patelrohanv.com/static/js/runtime~main.4d655a95.js"
+  },
+  {
+    "revision": "77cd364d616ac081184a",
+    "url": "patelrohanv.com/static/js/main.77cd364d.chunk.js"
+  },
+  {
+    "revision": "5781e0c5895ccf41cabf",
+    "url": "patelrohanv.com/static/js/1.5781e0c5.chunk.js"
+  },
+  {
+    "revision": "77cd364d616ac081184a",
+    "url": "patelrohanv.com/static/css/main.61cf8df9.chunk.css"
+  },
+  {
+    "revision": "5781e0c5895ccf41cabf",
+    "url": "patelrohanv.com/static/css/1.5a096335.chunk.css"
+  },
+  {
+    "revision": "c134cb7a5dd4d2e426d6805b89450009",
+    "url": "patelrohanv.com/index.html"
+  }
+];
