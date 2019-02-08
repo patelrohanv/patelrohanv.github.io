72fc61a69ede97e719265cf09fe1c25997d7c491
rohan <patelrohanv@gmail.com>
2019-02-08 07:16:23 -0500

    build

---
+++ build/precache-manifest.a6626e089a3c35ab7ee96224fe8f2513.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "6d4dd2fd2ef4f5ee7ad35db29aab75a9",
+    "url": "/static/media/headshot.6d4dd2fd.jpg"
+  },
+  {
+    "revision": "dfecbb8591923ca22621a76d6a1ad3dc",
+    "url": "/static/media/desk_light_l.dfecbb85.jpeg"
+  },
+  {
+    "revision": "d7c071b4fc64d8d9bf8e78b6150b8a47",
+    "url": "/static/media/code_m.d7c071b4.jpeg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "926d099beeaf44dedbca",
+    "url": "/static/js/main.926d099b.chunk.js"
+  },
+  {
+    "revision": "5781e0c5895ccf41cabf",
+    "url": "/static/js/1.5781e0c5.chunk.js"
+  },
+  {
+    "revision": "926d099beeaf44dedbca",
+    "url": "/static/css/main.61cf8df9.chunk.css"
+  },
+  {
+    "revision": "5781e0c5895ccf41cabf",
+    "url": "/static/css/1.5a096335.chunk.css"
+  },
+  {
+    "revision": "3815808087a303ec222e46bc20b2da72",
+    "url": "/index.html"
+  }
+];
